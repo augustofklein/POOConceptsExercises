503dfbad01d2c03b870983dc1eb8df592258dd7e
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2023-02-19 15:45:47 -0300

    Cricao do projeto Dictionary

---
+++ Course/Dictionary/Entities/Candidate.cs
@@ -0,0 +1,15 @@
+﻿namespace Dictionary.Entities
+{
+	public class Candidate
+	{
+        public string Name { get; set; }
+        public int Votes { get; set; }
+
+        public Candidate(string name, int votes)
+        {
+            Name = name;
+            Votes = votes;
+        }
+    }
+}
+
