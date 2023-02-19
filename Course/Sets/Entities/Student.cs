26d072021f256e13ae9fbf9eb07fd3683980ed3b
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2023-02-19 15:50:33 -0300

    Cricao do projeto Sets

---
+++ Course/Sets/Entities/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace Sets.Entities
+{
+	public class Student
+	{
+        public int Number { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Number.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Student))
+            {
+                return false;
+            }
+
+            Student other = obj as Student;
+
+            return Number.Equals(other.Number);
+        }
+    }
+}
+
---
+++ Course/Sets/Program.cs
@@ -0,0 +1,44 @@
+﻿using Sets.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Student> students = new HashSet<Student>();
+
+            Console.Write("How many students for course A? ");
+            int qtdStudentsA = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtdStudentsA; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                students.Add(new Student { Number = number });
+            }
+
+            Console.Write("How many students for course B? ");
+            int qtdStudentsB = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtdStudentsB; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                students.Add(new Student { Number = number });
+            }
+
+            Console.Write("How many students for course C? ");
+            int qtdStudentsC = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtdStudentsC; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                students.Add(new Student { Number = number });
+            }
+
+            Console.WriteLine("Total students: " + students.Count);
+        }
+    }
+}
