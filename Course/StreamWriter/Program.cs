16f53b70ba524e1cd6f0c6f8a454e333db1094b8
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2023-02-20 20:54:51 -0300

    Criacao do projeto StreamWriter

---
+++ Course/StreamWriter/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"/Users/augusto/Projects/POOConceptsExercises/Course/StreamWriter/file1.txt";
+            string targetPath = @"/Users/augusto/Projects/POOConceptsExercises/Course/StreamWriter/file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line);
+                    }
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error acurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
