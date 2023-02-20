2165443a6a335a5a53557c44f65e37bd67ef8596
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2023-02-20 11:36:18 -0300

    Criacao do projeto ScheduleException

---
+++ Course/ScheduleException/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿namespace ScheduleException.Exceptions
+{
+    public class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
+
