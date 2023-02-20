571cdd503177780f78ca3955fc7c032992088c5f
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2023-02-20 12:09:58 -0300

    Criacao do projeto ContractInterface

---
+++ Course/ContractInterface/Services/ContractService.cs
@@ -0,0 +1,28 @@
+﻿using ContractInterface.Entities;
+
+namespace ContractInterface.Services
+{
+	class ContractService
+	{
+        private IOnlinePaymentService _onlinePaymentService;
+
+        public ContractService(IOnlinePaymentService onlinePaymentService)
+        {
+            _onlinePaymentService = onlinePaymentService;
+        }
+
+        public void ProcessContract(Contract contract, int months)
+        {
+            double basixQuota = contract.TotalValue / months;
+            for (int i = 1; i <= months; i++)
+            {
+                DateTime date = contract.Date.AddMonths(i);
+                double updateQuota = basixQuota + _onlinePaymentService.Interest(basixQuota, i);
+                double fullQuota = updateQuota + _onlinePaymentService.PaymentFee(updateQuota);
+
+                contract.AddInstallment(new Installment(date, fullQuota));
+            }
+        }
+    }
+}
+
