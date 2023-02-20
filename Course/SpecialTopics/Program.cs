51450426b522bbad7b9741a2a172da9a03432af9
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2023-02-20 12:21:25 -0300

    Criacao do projeto SpecialTopics

---
+++ Course/SpecialTopics/Program.cs
@@ -0,0 +1,17 @@
+﻿using SpecialTopics.Extensions;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023, 02, 01, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morning dear students!";
+
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
