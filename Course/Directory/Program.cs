c3e28cc1126d9815a45eac700edd87c111383228
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2023-02-20 20:26:51 -0300

    Criacao do projeto Directory

---
+++ Course/Directory/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/Users/augusto/Projects/POOConceptsExercises/Course/Directory";
+
+            StreamReader sr = null;
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS");
+
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES");
+
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error acurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
