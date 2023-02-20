7ce263e934fae771ecbc0e39ac4297781b7cabcd
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2023-02-20 20:40:00 -0300

    Criacao do projeto FileStream

---
+++ Course/FileStream/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/Users/augusto/Projects/POOConceptsExercises/Course/FileStream/file1.txt";
+
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error acurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
