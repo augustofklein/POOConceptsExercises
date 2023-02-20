a26cc93eecc433aab7da001fc5ed43cc3f7483d5
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2023-02-20 20:35:08 -0300

    Criacao do projeto FileInfo

---
+++ Course/FileInfo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"/Users/augusto/Projects/POOConceptsExercises/Course/FileInfo/file1.txt";
+            string targetPath = @"/Users/augusto/Projects/POOConceptsExercises/Course/FileInfo/file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error accuurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
