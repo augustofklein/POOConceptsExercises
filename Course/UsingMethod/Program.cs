50f4de78883e69b531336aef51c56578ffebf021
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2023-02-20 20:58:59 -0300

    Criacao do projeto UsingMethod

---
+++ Course/UsingMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/Users/augusto/Projects/POOConceptsExercises/Course/UsingMethod/file1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
